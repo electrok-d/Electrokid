8bee11fa0b219dea3ffeec3e1468bc1b4b1fc220
Jed Black <62964873+electrok-d@users.noreply.github.com>
2021-06-09 20:13:22 -0700

    finished audio player

--- JS/howlPlayer.js
+++ JS/howlPlayer.js
@@ -5,9 +5,22 @@ let playerName;
 let playerMixIndex;
 let mixIndex;
 let mix;
+let progBarWidth = $('#progress').width();
+
+const progBarJump = () => {
+  $('#progressBar').width(`${(progBarWidth / sound.duration()) * sound.seek()}px`);
+};
 
 const progressBarAnimation = () => {
-  $('#progressBar').animate({width: "100%"},{duration: sound.duration()*1000, easing: 'linear'});
+  setInterval(() => {
+    updateWidth(); 
+  },sound.duration() - sound.seek() * 1000);
+  
+  function updateWidth() {
+    progBarWidth = $('#progress').width();
+    $('#progressBar').width(`${(progBarWidth / sound.duration()) * sound.seek()}px`);
+  }
+  
 };
 
 const playerId = (x) => {
@@ -28,21 +41,19 @@ const playerId = (x) => {
     src: [`https://www.electrokid.com/audio/${mix}.mp3`],
     html5: true,
     pool: 5,
-    // preload: false,
     onplay: function () {
       $(document).ready(function () {
-        
         const convertTime = () => {
           const seconds = Math.floor(sound.seek());
           const minutes = Math.floor(sound.seek() / 60);
           const minutesMod = minutes % 60;
           const hours = Math.floor(minutes / 60);
 
-          if (minutes > 59 )  {
+          if (minutes > 59) {
             return `${hours < 10 ? '0' : ''}${hours}:${`0${minutesMod}`}:${seconds - minutes * 60 < 10 ? '0' : ''}${seconds < 60 ? seconds : seconds - minutes * 60}`;
-            } else {
+          } else {
             return `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds - minutes * 60 < 10 ? '0' : ''}${seconds < 60 ? seconds : seconds - minutes * 60}`;
-           }
+          }
         };
 
         const convertDuration = () => {
@@ -50,39 +61,43 @@ const playerId = (x) => {
           const minutes = Math.floor(sound.duration() / 60);
           const minutesMod = minutes % 60;
           const hours = Math.floor(minutes / 60);
-          
-          if (minutes > 59 )  {
-          return `${hours < 10 ? '0' : ''}${hours}:${`0${minutesMod}`}:${seconds - minutes * 60 < 10 ? '0' : ''}${seconds < 60 ? seconds : seconds - minutes * 60}`;
+
+          if (minutes > 59) {
+            return `${hours < 10 ? '0' : ''}${hours}:${`0${minutesMod}`}:${seconds - minutes * 60 < 10 ? '0' : ''}${seconds < 60 ? seconds : seconds - minutes * 60}`;
           } else {
-          return `${hours < 10 ? '0' : ''}${hours}:${`${minutes}`}:${seconds - minutes * 60 < 10 ? '0' : ''}${seconds < 60 ? seconds : seconds - minutes * 60}`;
-         }
-          
+            return `${hours < 10 ? '0' : ''}${hours}:${`${minutes}`}:${seconds - minutes * 60 < 10 ? '0' : ''}${seconds < 60 ? seconds : seconds - minutes * 60}`;
+          }
         };
 
         setInterval(() => {
           $('#counter').text(`${convertTime()} / ${convertDuration()}`);
-          
         }, sound.duration() / 1000);
+        progressBarAnimation();
       });
-
-      progressBarAnimation();
     },
     onpause: function () {
-      $('#progressBar').clearQueue();
-      $('#progressBar').stop();
     },
-    onstop: function () {},
+    onseek: function () {
+      progBarJump();
+    },
     onend: function () {
-      console.log('Finished!');
+      console.log('Thanks for listening!');
     },
   });
-  playerHtml();
-  loadButtonObjects();
 
   $(document).ready(function () {
-    $('#counter').text(`00:00:00 / ${player.mixList[`${mixIndex}`].mixLength}`)
+  playerHtml();
   });
-    };
+
+  $(document).ready(function () {
+  loadButtonObjects();
+  });
+
+
+  $(document).ready(function () {
+    $('#counter').text(`00:00:00 / ${player.mixList[`${mixIndex}`].mixLength}`);
+  });
+};
 
 const playerHtml = () => {
   $(document).ready(function () {
@@ -102,7 +117,6 @@ const playerHtml = () => {
         '</div>' +
         "<div id='counter'></div>"
     );
-    
   });
 };
 
@@ -116,16 +130,10 @@ Player.prototype = {
     sound.play();
   },
   pause: function () {
-    $('#progressBar').clearQueue();
-    $('#progressBar').stop();
     sound.pause();
   },
   seek: function (x) {
-    $('#progressBar').clearQueue();
-    $('#progressBar').stop();
     x > 0 ? sound.seek(sound.seek() + x) : sound.seek(sound.seek() + x);
-    const currentProgBarWidth = $('#progress').width();
-    $('#progressBar').width(`${(currentProgBarWidth / sound.duration()) * sound.seek()}px`);
   },
   stop: function () {
     sound.stop();
@@ -137,30 +145,30 @@ const player = new Player([
     mixIndex: 0,
     title: 'Electrokid - The Robot Within',
     robotwithin: 'electrokid_robotwithin_256kbps',
-    mixLength: "01:02:00",
+    mixLength: '01:02:00',
     howl: null,
   },
   {
     mixIndex: 1,
     title: 'Jed Black - Disorderly Conduct Radio Show - April 6, 2016',
     disorderly: 'Jed Black_Disorderly Conduct Radio_6April2016',
-    mixLength: "00:59:29",
+    mixLength: '00:59:29',
     howl: null,
   },
   {
     mixIndex: 2,
     title: 'Jed Black - Mixed live on Kboo Radio',
     kboo: 'JedBlack_KbooRadio_Live_320kbps',
-    mixLength: "00:57.54",
+    mixLength: '00:57.54',
     howl: null,
   },
   {
     mixIndex: 3,
     title: 'Electrokid - Live At Whoadang - November 15, 2013',
     whoadang: 'Electrokid - Live at Whoadang - 15Nov2013',
-    mixLength: "01:00:41",
+    mixLength: '01:00:41',
     howl: null,
-  },  
+  },
 ]);
 
 function loadButtonObjects() {
@@ -174,6 +182,7 @@ function loadButtonObjects() {
     $('#play').on('click', () => {
       if (!sound.playing()) {
         player.play();
+        
       }
     });
   });
@@ -190,5 +199,15 @@ function loadButtonObjects() {
     $('#seekFwd').mousedown(() => {
       player.seek(30);
     });
-  });  
+  });
+
+  $(document).ready(function () {
+    $('#progress').mousedown(function (event) {
+      progBarWidth = $('#progress').width();
+      const relX = event.pageX - $(this).offset().left;
+      const seekNum = `${sound.duration()}` / `${progBarWidth}`;
+      sound.seek(relX * seekNum);
+      progBarJump();
+    });
+  });
 }
